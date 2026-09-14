eac60d5e58dd79f2c497226d51c25abe45f5ff2a
joankl <joankphysic@gmail.com>
2026-09-14 17:17:52 +0100

    updating

--- rat_v8/neutrons/macros/submit_neutron_simulation.sh
+++ rat_v8/neutrons/macros/submit_neutron_simulation.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-#SBATCH --job-name=RAT_neutron_simulation      # Nombre del trabajo
-#SBATCH --output=logs/analysis_%j.out  # Archivo de salida estándar (%j = Job ID)
-#SBATCH --error=logs/analysis_%j.err   # Archivo de errores
+#BATCH --job-name=RAT_neutron_simulation      
+#SBATCH --output=logs/simu.out  
+#SBATCH --error=logs/simu.err   
+#SBATCH --partition=lipq               # Partición estándar
 
-# 1. Cargar el entorno de RAT/ROOT
-# Reemplaza esta ruta con el archivo env.sh oficial de tu instalación de RAT 8
-source /lstore/sno/joankl/rat_v8/env.sh 
+echo "Running on host: $(hostname)"
+echo "Starting Apptainer Container for RAT Simulation..."
 
-# 2. Ir al directorio donde está tu script (opcional pero recomendado)
-cd /lstore/sno/joankl/rat_tests/rat_v8/neutrons/macros
+# --- Configuración de Rutas ---
+MACRO_DIR="/lstore/sno/joankl/rat_tests/rat_v8/neutrons/macros"
+CONTAINER_SIF="/lstore/sno/joankl/RAT/containers/rat_8.1.0.sif"
 
-# 3. Ejecutar el script de análisis
-echo "Initializing RAT Simulation"
-rat neutron_validation.mac 
-echo "Simulation Finished"
+# --- Comandos a ejecutar DENTRO del contenedor ---
+# Cargamos el entorno nativo de RAT dentro de la imagen, nos movemos al directorio y ejecutamos RAT
+COMMAND_INSIDE="source /usr/local/bin/geant4.sh && \
+                source /root-bin/bin/thisroot.sh && \
+                source /rat/env.sh && \
+                cd ${MACRO_DIR} && \
+                echo 'Initializing RAT Simulation' && \
+                rat neutron_validation.mac && \
+                echo 'Simulation Finished'"
+
+# --- Ejecución de Apptainer ---
+# Montamos /lstore para que el contenedor pueda leer tu macro y guardar el archivo .root resultante
+apptainer exec \
+    -B /share/neutrino/snoplus/ \
+    -B /lstore \
+    ${CONTAINER_SIF} \
+    bash -c "${COMMAND_INSIDE}"
